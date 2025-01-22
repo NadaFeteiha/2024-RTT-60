56291a7b6ca85fb801652cfc67ab8beee86af749
Nada Feteiha <nada.feteiha@gmail.com>
2025-01-22 08:22:38 -0800

    solve functions activities

--- Mod_308/javascript-intro/funcions.js
+++ Mod_308/javascript-intro/funcions.js
@@ -1,8 +1,62 @@
-//fun
+//Functions 
+// Lesson: https://ps-react-curriculum.herokuapp.com/308H/5/lesson
 
 const sum = (a, b) => a + b;
 
 //function call
 sum(1, 2);
+
+// 1.Function Declaration
+function sayHello(name) {
+    console.log('Hello ' + name + '!');
+}
+sayHello('World');
+
+//========================================
+
+// 2.Function Expression
+const sayHello = function (name) {
+    console.log('Hello ' + name + '!');
+}
+sayHello('World');
+
+//========================================
+
+// 3.Arrow Function
+const sayHello = (name) => {
+    console.log('Hello ' + name + '!');
+}
+sayHello('World');
+
+//========================================
+
+// 4. Default Parameters
+function sayHello(name = 'World') {
+    console.log('Hello ' + name + '!');
+}
+sayHello();
+
+//========================================
+
+// 5. Rest Parameters
+function sum(...numbers) {
+    return numbers.reduce((total, number) => total + number, 0);
+}
+console.log(sum(1, 2, 3, 4, 5));
+
+//========================================
+
+// 6. Spread Operator
+const numbers = [1, 2, 3, 4, 5];
+console.log(sum(...numbers));
+
+//========================================
+// Exercise
+function areBothEven(n1, n2) {
+    return !(n1 % 2) && !(n2 % 2);
+}
+
+console.log(`are both even 1 and 3? `, areBothEven(1, 3));
+//================================================
 
 
---
+++ Mod_308/javascript-intro/functions_activities.js
@@ -0,0 +1,28 @@
+console.log("================================================");
+console.log("Practice Activity 1");
+// Practice Activity 1: Write a Function Declaration
+// Write a function named computeArea using the function declaration approach.
+function computeArea(width, height) {
+    return width * height;
+}
+
+let width = 5;
+let height = 10;
+console.log(`The area of a rectangle with a width of ${width} and a height of ${height} is ${computeArea(width, height)} square units.`);
+//================================================
+console.log("================================================");
+console.log("Practice Activity 2");
+// Practice Activity 2: Write a Function Expression
+// Write a function named planetHasWater using the function expression syntax.
+const planetHasWater = function (planet) {
+    planet = planet.trim().toUpperCase();
+    return planet === 'EARTH' || planet === 'MARS';
+};
+
+console.log(`Does Earth have water? ${planetHasWater('Earth')}`);
+console.log(`Does EArth have water? ${planetHasWater('EArth')}`);
+console.log(`Does Venus have water? ${planetHasWater('Venus')}`);
+console.log(`Does Mars with spaces have water? ${planetHasWater('Mars   ')}`);
+console.log(`Does MARS with spaces have water? ${planetHasWater('MARS')}`);
+
+
