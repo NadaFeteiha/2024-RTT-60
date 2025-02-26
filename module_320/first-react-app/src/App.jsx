74a63be69ccf9c21b342b991688299c234f20efd
Nada Feteiha <nada.feteiha@gmail.com>
2025-02-26 08:19:44 -0800

    add learner

--- module_320/first-react-app/src/App.jsx
+++ module_320/first-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Learner from "./components/Learner";
 
 function App() {
   return (
     <div>
       <h1> First react App</h1>
       <NavBar></NavBar>
+      <Learner></Learner>
       <Footer></Footer>
     </div>
   );
