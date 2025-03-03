4782354a80e95d8db49449f13e6395076381df3a
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-03 06:40:46 -0800

    add lab thinking in react

--- module_320/first-react-app/src/App.jsx
+++ module_320/first-react-app/src/App.jsx
@@ -1,16 +1,23 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Learner from "./components/Learner";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
+const routes = [
+  {
+    path: "/",
+    element: <HomePage />,
+  }
+]
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <div>
-      <h1> First react App</h1>
-      <NavBar></NavBar>
-      <Learner></Learner>
-      <Footer></Footer>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ module_320/first-react-app/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Learner from "../components/Learner";
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1> First react App</h1>
+            <NavBar></NavBar>
+            <Learner></Learner>
+            <Footer></Footer>
+        </div>
+    );
+}
