497a03e87292a073f04b3ea71bab32948b324708
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-04 11:08:24 -0800

    add using ref

---
+++ module_320/using-ref/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useRef, useEffect } from "react";
+import stock from "./assets/stock.mov";
+import { createConnection } from "./data/chat";
+
+
+/*export default function VideoPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const myRef = useRef(null);
+
+  function handleClick() {
+    const nextIsPlaying = !isPlaying;
+    setIsPlaying(nextIsPlaying);
+
+    if (nextIsPlaying) {
+      myRef.current.play();
+    } else {
+      myRef.current.pause();
+    }
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</button>
+      <video
+        width="75%"
+        ref={myRef}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      >
+        <source src={stock} type="video/mp4" />
+      </video>
+    </>
+  );
+}
+*/
+
+export default function ChatRoom() {
+  useEffect(() => {
+    const connection = createConnection();
+    connection.connect();
+  }, []);
+  
+  return <h1>Welcome to the chat!</h1>;
+}
