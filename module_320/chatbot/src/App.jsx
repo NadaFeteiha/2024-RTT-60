3d7c36d9db798aa8a0d03299e1dedbbb47a45c6c
Nada Feteiha <nada.feteiha@gmail.com>
2025-03-07 11:49:25 -0800

    init add the code and api for chatbot

--- module_320/chatbot/src/App.jsx
+++ module_320/chatbot/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [prompt, setPrompt] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newMessages = {
+      role: 'user',
+      content: prompt,
+    }
+
+    // send the messages....
+
+
+    setMessages([...messages, newMessages]);
+    setPrompt('');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="flex bg-indigo-900 text-white h-screen">
+      <section className="flex-1 border p-5">
+        <h1>Chatbot</h1>
+      </section>
+
+      <section className="flex-3 border p-5">
+        <div className="flex flex-col justify-between">
+          <div>
+            {messages.map((message, idx) => {
+              return (
+                <div key={idx}>
+                  <div>{message.content}</div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+
+        <form onSubmit={handleSubmit} className="flex justify-between w-full">
+          <input
+            type="text"
+            className="border w-full p-1"
+            placeholder="Prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <input type="submit" className="ml-2 border p-1" />
+        </form>
+      </section>
+    </main>
+  );
+
 }
 
 export default App
